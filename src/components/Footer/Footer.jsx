03102cde7fda6447376db903658ae385ebcdef99
Ian <ianpelina42@gmail.com>
2024-02-24 14:34:25 +0100

    update footer links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,10 +5,10 @@ export default function Footer() {
         <footer style={{zIndex: 999}}>
             <ul>
                 <li>
-                    <a aria-label='git-logo' alt='git' href="https://github.com/"><i className="fab fa-github icon"></i></a>
+                    <a aria-label='git-logo' alt='git' href="https://github.com/IanPelina"><i className="fab fa-github icon"></i></a>
                 </li>
                 <li>
-                    <a aria-label='linkedin-logo' alt='linkedin' href="https://www.linkedin.com/in/ian-le-pape-7290ba2a4/"><i className="fab fa-linkedin-in icon"></i></a>
+                    <a aria-label='linkedin-logo' alt='linkedin' href="https://www.linkedin.com/in/ian-le-pape"><i className="fab fa-linkedin-in icon"></i></a>
                 </li>
             </ul>
             <div>
